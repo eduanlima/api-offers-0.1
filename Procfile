0dbda0b1f87353d71b1f3a5f23d3a8e557449f79
eduanlima <eduanlima@outlook.com>
2020-03-11 11:42:27 -0300

    Created class ApiOfferMain

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java $JAVA_OPTS -cp target/classes:target/dependency/* br.com.services
+web: java $JAVA_OPTS -cp target/classes:target/dependency/* ApiOffersMain
---
+++ src/ApiOffersMain.java
@@ -0,0 +1,11 @@
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("v01")
+public class ApiOffersMain extends Application {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
