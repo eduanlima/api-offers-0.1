bdc44eac57b2e598d2f77fef56f0fb95ec3d9557
eduanlima <eduanlima@outlook.com>
2020-04-06 10:08:07 -0300

    List all stores

--- src/br/com/services/ServicesStoreAPI.java
+++ src/br/com/services/ServicesStoreAPI.java
@@ -1,10 +1,13 @@
 package br.com.services;
 
+import java.util.List;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 import br.com.model.entities.Store;
 import br.com.model.services.StoreService;
 
@@ -14,12 +17,21 @@ public class ServicesStoreAPI {
 	private static final String CHARSET_UTF8 = ";charset=utf-8";
 	
 	@GET
+	@Produces(MediaType.APPLICATION_JSON + CHARSET_UTF8)
+	public List<Store> findAll(){
+		List<Store> stores;
+		StoreService storeService = new StoreService();
+		stores = storeService.findAll();
+		return stores;
+	}
+	
+	
+	@GET
 	@Path("/{id_store}")
 	@Produces(MediaType.APPLICATION_JSON + CHARSET_UTF8)
 	public Store find(@PathParam("id_store") Integer id_store){
 		Store store;
-		StoreService storeService;
-		storeService = new StoreService();
+		StoreService storeService = new StoreService();
 		store = new Store();
 		store = storeService.find(id_store);
 		return store;
